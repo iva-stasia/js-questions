1f65c1d81d0571a799d08a5fc5a2955b95bacf65
Anastasiia Ivanchenko <iva21sta@gmail.com>
2023-03-08 20:33:07 +0200

    Move questions to the questions.js

---
+++ questions.js
@@ -0,0 +1,195 @@
+const questions = [
+    {
+        text: 'Explain event delegation.',
+        isLearned: true,
+    },
+    {
+        text: 'Explain how "this" works in JavaScript.',
+        isLearned: true,
+    },
+    {
+        text: 'Can you give an example of one of the ways that working with "this" has changed in ES6?',
+        isLearned: false,
+    },
+    {
+        text: 'Explain how prototypal inheritance works.',
+        isLearned: false,
+    },
+    {
+        text: 'What\'s the difference between a variable that is: "null", "undefined" or undeclared?',
+        isLearned: true,
+    },
+    {
+        text: 'How would you go about checking for any of states "null", "undefined" or undeclared?',
+        isLearned: true,
+    },
+    {
+        text: 'What is a closure, and how/why would you use one?',
+        isLearned: false,
+    },
+    {
+        text: 'What language constructions do you use for iterating over object properties and array items?',
+        isLearned: true,
+    },
+    {
+        text: 'Can you describe the main difference between the Array.forEach() loop and Array.map() methods and why you would pick one versus the other?',
+
+        isLearned: true,
+    },
+    {
+        text: "What's a typical use case for anonymous functions?",
+        isLearned: true,
+    },
+    {
+        text: "What's the difference between host objects and native objects?",
+        isLearned: false,
+    },
+    {
+        text: 'Explain the difference between: function Person(){}, var person = Person(), and var person = new Person()?',
+        isLearned: true,
+    },
+    {
+        text: 'Explain the differences on the usage of foo between function foo() {} and var foo = function() {}',
+        isLearned: true,
+    },
+    {
+        text: "Can you explain what Function.call and Function.apply do? What's the notable difference between the two?",
+        isLearned: true,
+    },
+    {
+        text: 'Explain Function.prototype.bind.',
+        isLearned: false,
+    },
+    {
+        text: "What's the difference between feature detection, feature inference, and using the UA string?",
+        isLearned: true,
+    },
+    {
+        text: 'Explain "hoisting".',
+        isLearned: true,
+    },
+    {
+        text: 'Describe event bubbling.',
+        isLearned: false,
+    },
+    {
+        text: 'Describe event capturing.',
+        isLearned: true,
+    },
+    {
+        text: 'What\'s the difference between an "attribute" and a "property"?',
+        isLearned: false,
+    },
+    {
+        text: 'What are the pros and cons of extending built-in JavaScript objects?',
+        isLearned: true,
+    },
+    {
+        text: 'What is the difference between == and ===?',
+        isLearned: true,
+    },
+    {
+        text: 'Explain the same-origin policy with regards to JavaScript.',
+        isLearned: true,
+    },
+    {
+        text: 'Why is it called a Ternary operator, what does the word "Ternary" indicate?',
+        isLearned: false,
+    },
+    {
+        text: 'What is strict mode? What are some of the advantages/disadvantages of using it?,',
+        isLearned: true,
+    },
+    {
+        text: 'What are some of the advantages/disadvantages of writing JavaScript code in a language that compiles to JavaScript?',
+        isLearned: false,
+    },
+    {
+        text: 'What tools and techniques do you use debugging JavaScript code?',
+        isLearned: true,
+    },
+    {
+        text: 'Explain the difference between mutable and immutable objects.',
+        isLearned: true,
+    },
+    {
+        text: 'What is an example of an immutable object in JavaScript?',
+        isLearned: false,
+    },
+    {
+        text: 'What are the pros and cons of immutability?',
+        isLearned: true,
+    },
+    {
+        text: 'How can you achieve immutability in your own code?',
+        isLearned: false,
+    },
+    {
+        text: 'Explain the difference between synchronous and asynchronous functions.',
+        isLearned: true,
+    },
+    {
+        text: 'What is event loop?',
+        isLearned: true,
+    },
+    {
+        text: 'What is the difference between call stack and task queue?',
+        isLearned: true,
+    },
+    {
+        text: 'What are the differences between variables created using "let", "var" or "const"?',
+        isLearned: true,
+    },
+    {
+        text: 'What are the differences between ES6 class and ES5 function constructors?',
+        isLearned: false,
+    },
+    {
+        text: 'Can you offer a use case for the new arrow => function syntax? How does this new syntax differ from other functions?',
+        isLearned: true,
+    },
+    {
+        text: 'What advantage is there for using the arrow syntax for a method in a constructor?',
+        isLearned: false,
+    },
+    {
+        text: 'What is the definition of a higher-order function?',
+        isLearned: true,
+    },
+    {
+        text: 'Can you give an example for destructuring an object or an array?',
+        isLearned: true,
+    },
+    {
+        text: 'Can you give an example of generating a string with ES6 Template Literals?',
+        isLearned: true,
+    },
+    {
+        text: 'Can you give an example of a curry function and why this syntax offers an advantage?',
+        isLearned: false,
+    },
+    {
+        text: 'What are the benefits of using spread syntax and how is it different from rest syntax?',
+        isLearned: false,
+    },
+    {
+        text: 'How can you share code between files?',
+        isLearned: true,
+    },
+    {
+        text: 'Why you might want to create static class members?',
+        isLearned: true,
+    },
+    {
+        text: 'What is the difference between while and do-while loops in JavaScript?',
+        isLearned: true,
+    },
+    {
+        text: 'What is a promise? Where and how would you use promise?',
+        isLearned: false,
+    },
+    {
+        text: 'Discuss how you might use Object Oriented Programming principles when coding with JavaScript.',
+        isLearned: true,
+    },
+];
